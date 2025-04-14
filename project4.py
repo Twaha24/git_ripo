93f09e6f5ef5bebc5d79d05c9cc32d1f5c3de5fe
Twaha24 <twahastyles@gmail.com>
2025-04-14 13:31:58 +0300

    monday reforms

--- project4.py
+++ project4.py
@@ -1,16 +1,16 @@
 restaurant = []
 
 details = {
-        "number": "580",
-        "name":"JAVAS",
-        "address":"KAWEMPE",
+        "number": "001",
+        "name":"JAVAS RESTAURANT",
+        "address":"KAWEMPE, Bombo rd.",
         "contact person":"TWAHA",
         "contact":"0758102369",
-        "opening hour":"06:00",
-        "closing hour":"05:00"
+        "opening hour":"06:00 am",
+        "closing hour":"10:00 pm"
     }
 restaurant.append(details)
-
+meals = []
 while True:
     meal_no = input("Enter meal number or 'Q' to quit: ")
     if meal_no.upper()=='Q':
@@ -18,10 +18,11 @@ while True:
 
     meal_details = []
 
-    for i in range(5):
+    for i in range(4):
         dish = input("Enter Food: ")
         meal_details.append(dish)
-    
+    drink = input("Enter Drink: ")
+    meal_details.append(drink)
     price = int(input("PRICE = "))
     status = input("STATUS: ")
 
@@ -31,8 +32,16 @@ while True:
         "price":price,
         "status":status
     }
-    restaurant.append(mealdict)
+    meals.append(mealdict)
 
-print(restaurant)
+def display_restaurant_details():
+    for detail in restaurant:
+        print(f"\n=====JAVA RESTAURANT DETAILS=====\n")
+        print(f'No. :\t\t\t{details.get("number")}\nName :\t\t\t{details.get("name")}\nAddress :\t\t{details.get("address")}\nContact person : \t{details.get("contact person")}\nContact :\t\t{details.get("contact")}\nOpening Hours :\t\t{details.get("opening hour")}\nClosing Hour :\t\t{details.get("closing hour")}')
+        
+    
+
+print(display_restaurant_details())       
+
 
    
